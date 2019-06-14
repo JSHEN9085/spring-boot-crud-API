7a1ba72dfc557d6a8c774084439c6e8e207bee07
JSHEN9085 <jshen9085@gmail.com>
2019-06-14 11:45:50 -0400

    difine employee

---
+++ src/main/java/com/jshen/springboot/cruddemo/entity/Employee.java
@@ -0,0 +1,82 @@
+package com.jshen.springboot.cruddemo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+    //define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="last_name") //format of column is different
+    private String lastName;
+
+    @Column(name="first_name") ////format of column is different
+    private String firstName;
+
+    @Column(name="email")
+    private String email;
+
+
+    //define constructor
+    public Employee(){
+
+    } //Hibernate requires an empty constructor
+
+    public Employee(String lastName, String firstName, String email) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+
+    //defind setter/getter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //define toString
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
